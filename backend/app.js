dced9ca4d4fc0bf7d0065790ad82a4bcbc2f0522
Karthikaut <karthikkathiresan1995@gmail.com>
2025-04-11 13:54:17 +0530

    change today

--- backend/app.js
+++ backend/app.js
@@ -1,88 +1,63 @@
-// const express = require('express');
-// const db = require('./models');
-// const path = require('path');
-// const app = express();
-
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-
-// // Static files
-// app.use(express.static(path.join(__dirname, 'public')));
-
-// // View engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'pug');
-
-
-
-// app.use('/api/assets', require('./routes/assetRoutes'));
-// app.use('/api/employees', require('./routes/employeeRoutes'));
-// app.use('/api/categories', require('./routes/assetCategoryRoutes'));
-// app.use('/api/assets', require('./routes/assetRoutes'));
-// app.use('/api/transactions', require('./routes/transactionRoutes'));
-
-// app.use('/libs', express.static(path.join(__dirname, 'public/libs')));
-// app.use('/css', express.static(path.join(__dirname, 'public/css')));
-// app.use('/js', express.static(path.join(__dirname, 'public/js')));
-
-
-
-// app.get('/assets/list',async (req, res) => {
-//     const assets = await db.Asset.findAll();
-//     const employees = await db.Employee.findAll({});
-//     console.log(assets,employees,"name")
-//     res.render('assetList', { data: assets, data1: employees });
-// });
-
-// db.sequelize.sync({ force: false }).then(() => {
-//     app.listen(3000, () => console.log('Server is running on http://localhost:3000'));
-// });
-
-
-
-
-
 const express = require('express');
 const app = express();
 const cors = require('cors');
 const path = require('path');
-
-const { sequelize } = require('./models'); // Ensure models/index.js exports sequelize instance
+const { sequelize, Employee, Asset } = require('./models');
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Routes
+// Set view engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.get('/assets', async (req, res) => {
+    try {
+        const assets = await Asset.findAll({ include: ['assignedTo'] });
+        const employees = await Employee.findAll();
+        res.render('assets/list', { assets, employees });
+    } catch (err) {
+        res.status(500).send('Error loading assets');
+    }
+});
+
+app.get('/employees', async (req, res) => {
+    try {
+        const employees = await Employee.findAll();
+        console.log(employees,'employees')
+        res.render('employees/list', { employees });
+    } catch (err) {
+        res.status(500).send('Error loading employees');
+    }
+});
+
+
+// API Routes
 const employeeRoutes = require('./routes/employeeRoutes');
 const assetRoutes = require('./routes/assetRoutes');
 const assetCategoryRoutes = require('./routes/assetCategoryRoutes');
 const transactionRoutes = require('./routes/transactionRoutes');
 
-// Use Routes
-
-app.use('/employees', employeeRoutes);
-app.use('/assets', assetRoutes);
-app.use('/categories', assetCategoryRoutes);
+app.use('/api/employees', employeeRoutes);
+app.use('/api/assets', assetRoutes);
+app.use('/api/categories', assetCategoryRoutes);
 app.use('/api/transactions', transactionRoutes);
 
-// Default Route
+// Redirect root to UI
 app.get('/', (req, res) => {
-    res.send('Asset Management System API is running.');
+    res.render('home')
 });
 
-// Sync DB and Start Server
+// Start Server
 const PORT = process.env.PORT || 3000;
 
-sequelize.sync({ alter: true }) // use { force: true } to reset DB
-    .then(() => {
-        console.log('Database synced');
-        app.listen(PORT, () => {
-            console.log(`Server is running on http://localhost:${PORT}`);
-        });
-    })
-    .catch(err => {
-        console.error('Unable to connect to the database:', err);
+sequelize.sync({ alter: true }).then(() => {
+    console.log('Database synced');
+    app.listen(PORT, () => {
+        console.log(`Server is running on http://localhost:${PORT}`);
     });
-
+}).catch(err => {
+    console.error('Unable to connect to the database:', err);
+});
